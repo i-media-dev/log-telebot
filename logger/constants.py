cb704ffd95723353838df16e939b0709c5cc2f0f
i-media-dev <mrk_dev@i-media.ru>
2025-08-27 19:43:21 +0300

    delete test endpoints

--- logger/constants.py
+++ logger/constants.py
@@ -7,11 +7,11 @@ load_dotenv()
 
 PROJECTS = {
     'wb_parser': {
-        'log_path': os.getenv('TEST_LOG_PATH_WB'),
+        'log_path': os.getenv('LOG_PATH_WB'),
         'check_time': '00:30',
     },
     'citilink_feed_handler': {
-        'log_path': os.getenv('TEST_LOG_PATH_CITILINK'),
+        'log_path': os.getenv('LOG_PATH_CITILINK'),
         'check_time': '07:30',
     }
 }
