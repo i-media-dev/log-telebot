f28ec91a364dd76fde43df19181b6229365e4eb1
i-media-dev <mrk_dev@i-media.ru>
2025-09-03 19:51:56 +0300

    update

--- logger/webhook.py
+++ logger/webhook.py
@@ -37,10 +37,7 @@ class WebhookManager:
         def trigger_deploy():
             data = request.json
             project_name = data.get('project', 'проекта')
-
             message_str = f'✅ Деплой {project_name} успешно выполнен!'
-            if project_name == 'log-telebot':
-                message_str = '✌ Я обновился!'
 
             self.bot.send_message_str(os.getenv('TELEGRAM_TO'), message_str)
             self.bot.get_robot(DEPLOY_ROBOT, os.getenv('TELEGRAM_TO'))
