52e9384410cca720afb5c8d60d867557f5ac902c
i-media-dev <mrk_dev@i-media.ru>
2025-09-04 19:35:57 +0300

    upgrade

--- logger/watchdog.py
+++ logger/watchdog.py
@@ -1,3 +1,6 @@
+import os
+import time
+
 from watchdog.events import FileSystemEventHandler
 
 from logger.constants import PROJECTS
@@ -9,19 +12,29 @@ class LogFileHandler(FileSystemEventHandler):
         self.bot = bot
         self.projects = projects
         self.log_dir_to_project = {}
+        self.last_processed = {}
         for project_name, config in projects.items():
             log_dir = config['log_path']
             self.log_dir_to_project[log_dir] = project_name
 
     def on_closed(self, event):
         if not event.is_directory:
+            current_time = time.time()
+            file_path = event.src_path
+
+            if (file_path in self.last_processed and
+                    current_time - self.last_processed[file_path] < 30):
+                return
+
             project_name = self._get_project_from_event(event)
             if project_name:
+                self.last_processed[file_path] = current_time
                 self.bot.send_project_report(project_name)
 
     def _get_project_from_event(self, event):
-        file_path = event.src_path
+        file_dir = os.path.dirname(event.src_path)
+
         for log_dir, project_name in self.log_dir_to_project.items():
-            if file_path.startswith(log_dir):
+            if file_dir == log_dir:
                 return project_name
         return None
