19726402dd5a4931f360bebd98cc8c1b346f1b4f
i-media-dev <mrk_dev@i-media.ru>
2025-09-05 16:35:49 +0300

    ultimate fix3

--- logger/watchfile.py
+++ logger/watchfile.py
@@ -11,8 +11,6 @@ setup_logging()
 
 
 class LogFileHandler(FileSystemEventHandler):
-    """Обработчик событий файловых изменений для логов с debounce."""
-
     DEBOUNCE_SECONDS = 1
 
     def __init__(self, bot, projects: dict[str, dict] = PROJECTS):
@@ -21,6 +19,7 @@ class LogFileHandler(FileSystemEventHandler):
         self.log_dir_to_project = {}
         self.last_processed = {}
         self.debounce_timers = {}
+        self.already_reported = set()
 
         for project_name, config in projects.items():
             log_dir = config['log_path']
@@ -29,9 +28,9 @@ class LogFileHandler(FileSystemEventHandler):
     def on_closed(self, event):
         if event.is_directory or not event.src_path.endswith('.log'):
             return
-
         file_path = event.src_path
         project_name = self._get_project_from_path(file_path)
+
         if not project_name:
             return
 
@@ -39,6 +38,7 @@ class LogFileHandler(FileSystemEventHandler):
             mtime = os.path.getmtime(file_path)
         except FileNotFoundError:
             return
+
         last_mtime = self.last_processed.get(file_path)
 
         if last_mtime == mtime:
@@ -57,19 +57,22 @@ class LogFileHandler(FileSystemEventHandler):
         timer.start()
 
     def _process_file(self, file_path, project_name):
-        """Обрабатываем файл после debounce."""
         try:
             tag, _ = self.bot.log_monitor.check_logs(project_name)
             if tag not in ['SUCCESS', 'ERROR']:
-                logging.info(f'Файл {file_path} еще пишется, пропускаем')
+                logging.info(f'Файл {file_path} еще пишется')
                 return
+
+            if file_path in self.already_reported:
+                logging.info(f'Файл {file_path} уже обработан')
+                return
+            self.already_reported.add(file_path)
 
             logging.info(
                 f'Обнаружено изменение и готовность лога: {file_path}')
             self.bot.send_project_report(project_name)
         finally:
-            if file_path in self.debounce_timers:
-                del self.debounce_timers[file_path]
+            self.debounce_timers.pop(file_path, None)
 
     def _get_project_from_path(self, file_path):
         file_dir = os.path.dirname(file_path)
