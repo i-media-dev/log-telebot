2685205d1eacb5296d09fdf2d47219420d137510
i-media-dev <mrk_dev@i-media.ru>
2025-09-11 19:15:20 +0300

    fallback

--- logger/main.py
+++ logger/main.py
@@ -8,17 +8,12 @@ from logger.webhook import WebhookManager
 load_dotenv()
 
 
-def main():
-    bot = IBotLog(os.getenv('TELEGRAM_TOKEN'))
-    webhook_manager = WebhookManager(
-        bot=bot,
-        telebot=bot.bot,
-        host=os.getenv('WEBHOOK_HOST')
-    )
+bot = IBotLog(os.getenv('TELEGRAM_TOKEN'))
+webhook_manager = WebhookManager(
+    bot=bot,
+    telebot=bot.bot,
+    host=os.getenv('WEBHOOK_HOST')
+)
 
-    webhook_manager.setup_webhook()
-    return webhook_manager.get_app()
-
-
-if __name__ == '__main__':
-    app = main()
+webhook_manager.setup_webhook()
+app = webhook_manager.get_app()
